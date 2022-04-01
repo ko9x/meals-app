c7bca7de6d6436a42ed94683beed6911c41c171e
Sean McGillis <slcidevice@yahoo.com>
2022-03-31 20:57:30 -0600

    Added some new dummy data and started styling the CategoryMealsScreen

--- components/UI/Screen.js
+++ components/UI/Screen.js
@@ -11,5 +11,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        margin: 20,
     }
 });
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,30 +1,71 @@
-import { StyleSheet, View, Text, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import Screen from "../components/UI/Screen";
-import { CATEGORIES } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-export default function CategoryMealsScreen(props,) {
-  const catId = props.navigation.getParam('categoryId');
+export default function CategoryMealsScreen(props) {
+  const catId = props.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const onSelectMeal = () => {
+  };
+
+  const renderMealItem = (itemData) => {
+    return (
+      <View style={styles.mealItem}>
+        <TouchableOpacity onPress={() => {onSelectMeal}}>
+        <View style={{...styles.mealRow, ...styles.mealHeader}}>
+          <Text>{itemData.item.title}</Text>
+        </View>
+        <View style={{...styles.mealRow, ...styles.mealDetail}}>
+          <Text>{itemData.item.duration}m</Text>
+          <Text>{itemData.item.complexity}</Text>
+          <Text>{itemData.item.affordability}</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <Screen>
-      <Text>The Category Meals Screen!</Text>
-      <Button title="To Meal Detail Screen!" onPress={() => {
-          props.navigation.navigate('MealDetail')
-      }}/>
+      <FlatList style={{width: '100%'}} data={displayedMeals} renderItem={renderMealItem} />
     </Screen>
   );
 }
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
-  const catId = navigationData.navigation.getParam('categoryId');
+  const catId = navigationData.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  // const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
-  return {
-    headerTitle: selectedCategory.title,
-  };
+  // return {
+  //   headerTitle: selectedCategory.title,
+  // };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: '100%',
+    backgroundColor: '#ccc',
+  },  
+  mealRow: {
+    flexDirection: 'row',
+  },
+  mealHeader: {
+    height: '90%',
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+  }
+});
