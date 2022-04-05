338ce9dbdb3c522ef493ac5eda6149af6b1541cc
Sean McGillis <slcidevice@yahoo.com>
2022-04-04 21:12:20 -0600

    Added a favorites stack to the MealsNavigator and added a FlatList to Favorites to show some meals on the page

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -7,47 +7,73 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import Favorites from "../screens/Favorites";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+
+const stackNavOptions = {
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+}
 
 const MealsNavigator = createStackNavigator(
   {
     Categories: {
       screen: CategoriesScreen,
       navigationOptions: {
-          headerTitle: 'Meal Categories'
-      }
+        headerTitle: "Meal Categories",
+      },
     },
     CategoryMeals: CategoryMealsScreen,
     MealDetail: MealDetailScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
-      },
-      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
-    },
+    defaultNavigationOptions: stackNavOptions
   }
 );
 
-const TabNavigator = createBottomTabNavigator({
-    Meals: {screen: MealsNavigator, navigationOptions: {
+const FavNavigator = createStackNavigator(
+  {
+    Favorites: Favorites,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: stackNavOptions
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
         tabBarIcon: (tabInfo) => {
-            return <Ionicons name='ios-restaurant' size={30} color={tabInfo.tintColor} />
-        }
-    }},
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={30}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
     Favorites: {
-        screen: Favorites,
-        navigationOptions: {
-            tabBarIcon: (tabInfo) => {
-                return <Ionicons name='ios-star' size={30} color={tabInfo.tintColor} />
-            }
-        }
-    }
-}, {
+      screen: FavNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-star" size={30} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
     tabBarOptions: {
-        activeTintColor: Colors.secondary
-    }
-});
+      activeTintColor: Colors.secondary,
+    },
+  }
+);
 
 export default createAppContainer(TabNavigator);
--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,11 +1,31 @@
 import { StyleSheet, View, Text } from "react-native";
+import { FlatList } from "react-navigation";
 import Screen from "../components/UI/Screen";
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
 
 export default function Favorites() {
+  const dummyArray = MEALS.filter(
+    (meal) => meal.id === "m1" || meal.id === "m2"
+  );
+
+  const renderMeals = (itemData) => {
     return (
-        <Screen><Text>The Favorites Screen!</Text></Screen>
-    )
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <Screen>
+      <FlatList data={dummyArray} renderItem={renderMeals} style={{width: '100%'}} />
+    </Screen>
+  );
+}
+
+Favorites.navigationOptions = {
+  headerTitle: "Your Favorites",
 };
 
 const styles = StyleSheet.create({});
-
