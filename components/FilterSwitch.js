059abdcb8a61c932e2a18a95c42f97e8fddbf6f5
Sean McGillis <slcidevice@yahoo.com>
2022-04-06 14:55:27 -0600

    Created the FilterSwitch component

---
+++ components/FilterSwitch.js
@@ -0,0 +1,32 @@
+import { View, Text, Switch, Platform, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+
+export default function FilterSwitch(props) {
+  console.log("props", props); //@DEBUG
+
+  const toggleSwitch = (value) => {
+    props.onSwitch(value);
+  };
+
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primary }}
+        thumbColor={Platform.OS === "android" ? Colors.secondary : ""}
+        value={props.value}
+        onValueChange={(newValue) => props.onSwitch(newValue)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    paddingVertical: 15,
+  },
+});
