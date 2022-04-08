410f32869d998b1a8a57e5751bc7d4367db7d795
Sean McGillis <slcidevice@yahoo.com>
2022-04-07 21:17:08 -0600

    Added fallback text if no favorites have been selected

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -2,10 +2,20 @@ import MealList from "../components/MealList";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
 import { useSelector } from "react-redux";
+import { View, Text, StyleSheet } from "react-native";
+import Screen from "../components/UI/Screen";
+import DefaultText from "../components/UI/DefaultText";
 
 export default function Favorites(props) {
+  const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
 
-  const favoriteMeals = useSelector(state => state.meals.favoriteMeals)
+  if (favoriteMeals.length < 1) {
+    return (
+      <Screen>
+        <DefaultText>No favorite meals found. Add some!</DefaultText>
+      </Screen>
+    );
+  }
 
   return <MealList meals={favoriteMeals} navigation={props.navigation} />;
 }
@@ -13,15 +23,16 @@ export default function Favorites(props) {
 Favorites.navigationOptions = (navData) => {
   return {
     headerTitle: "Your Favorites",
-    headerLeft: () =>
-        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-          <Item
-            title="Menu"
-            iconName="ios-menu"
-            onPress={() => {
-              navData.navigation.toggleDrawer();
-            }}
-          />
-        </HeaderButtons>
-  }
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
